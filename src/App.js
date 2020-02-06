db27abe5ec55a2fbb2acac703956e67d5be5abde
Nicole <heeyeons1993@gmail.com>
2020-02-05 22:45:39 -0800

    Completed setting game to be able to take turns and determine winner function

--- src/App.js
+++ src/App.js
@@ -8,34 +8,40 @@ import './App.css';
   ===============================================*/
 
 
-
-class Square extends React.Component {
-  state = {
-    value: null
-  };
-
-  render() {
+ function Square(props) {
     return (
       <button
         className="square"
-        onClick={() => this.props.onClick()}>
-        {this.props.value}
+        onClick={props.onClick}>
+        {props.value}
       </button>
     );
   }
-}
+
 
 
 
 class Board extends React.Component {
   state = {
-    squares: Array(9).fill(null)
+    squares: Array(9).fill(null),
+    xIsNext: true
   };
 
   handleClick(i) {
+    // making a copy of this.state.square
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares});
+
+    // stop playing when winner is true
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    // taking turns, default by 'X' and next turn switch to 'O'
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
   }
 
   renderSquare(i) {
@@ -45,7 +51,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -88,6 +100,26 @@ class Game extends React.Component {
   }
 }
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 
 export default Game;
